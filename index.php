97218e4e36fa0b0abb871ba1c18afb2f90d4eeca
Arash-11 <arashnawy@gmail.com>
2021-02-06 10:29:51 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quizzer</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <header>
+    <div class="container">
+      <h1>Quizzer</h1>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+      <h2>Test your PHP knowledge</h2>
+    </div>
+  </main>
+  <footer>
+    <div class="container">
+      Copyright &copy; 2021, Quizzer
+    </div>
+  </footer>
+</body>
+</html>
